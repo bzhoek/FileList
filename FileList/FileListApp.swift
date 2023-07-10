a10d5c2e327983bfcf1bbe5ece6981a682bd9676
Bas van der Hoek <bas@hoek.com>
2023-07-10 12:09:26 +0200

    Initial Commit

---
+++ FileList/FileListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileListApp.swift
+//  FileList
+//
+//  Created by Bas van der Hoek on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FileListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
